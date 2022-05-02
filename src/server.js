c97ffec01d3ab435caad85ad13b14d449fa95aad
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2022-05-02 22:33:40 +0700

    dev

--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
 const Hapi = require('@hapi/hapi');
-const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
-const NotesValidator = require('./validator/notes');
+const routes = require('./routes');
 
 const init = async () => {
-  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -15,13 +12,7 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
-    },
-  });
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
